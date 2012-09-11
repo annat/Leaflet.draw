e47b37a5f5e5bf2fe8dae1671b379b5dc5e56ea3
annat <athieme@kth.se>
2012-09-11 09:08:36 +0200

    marker is draggable

--- src/draw/shapes/Marker.Draw.js
+++ src/draw/shapes/Marker.Draw.js
@@ -1,63 +1,62 @@
 L.Marker.Draw = L.Handler.Draw.extend({
-	options: {
-		icon: new L.Icon.Default()
-	},
+options: {
+icon: new L.Icon.Default()
+},
 
-	addHooks: function () {
-		L.Handler.Draw.prototype.addHooks.call(this);
+addHooks: function () {
+L.Handler.Draw.prototype.addHooks.call(this);
 
-		if (this._map) {
-			this._updateLabelText({ text: 'Click map to place marker.' });
-			this._map.on('mousemove', this._onMouseMove, this);
-		}
-	},
+if (this._map) {
+this._updateLabelText({ text: 'Click map to place marker.' });
+this._map.on('mousemove', this._onMouseMove, this);
+this._map.on('dragstart', dragstart);
+this._map.on('dragend', dragend);
+}
+},
 
-	removeHooks: function () {
-		L.Handler.Draw.prototype.removeHooks.call(this);
+removeHooks: function () {
+L.Handler.Draw.prototype.removeHooks.call(this);
 
-		if (this._map) {
-			if (this._marker) {
-				this._marker.off('click', this._onClick);
-				this._map
-					.off('click', this._onClick)
-					.removeLayer(this._marker);
-				delete this._marker;
-			}
+if (this._map) {
+if (this._marker) {
+this._marker.off('click', this._onClick);
+this._map
+.off('click', this._onClick)
+.removeLayer(this._marker);
+delete this._marker;
+}
 
-			this._map.off('mousemove', this._onMouseMove);
-		}
-	},
+this._map.off('mousemove', this._onMouseMove);
+}
+},
 
-	_onMouseMove: function (e) {
-		var newPos = e.layerPoint,
-			latlng = e.latlng;
+_onMouseMove: function (e) {
+var newPos = e.layerPoint,
+latlng = e.latlng;
 
-		this._updateLabelPosition(newPos);
+this._updateLabelPosition(newPos);
 
-		if (!this._marker) {
-			this._marker = new L.Marker(latlng, { icon: this.options.icon });
-			// Bind to both marker and map to make sure we get the click event.
-			this._marker.on('click', this._onClick, this);
+if (!this._marker) {
+this._marker = new L.Marker(latlng, {icon: this.options.icon });
+// Bind to both marker and map to make sure we get the click event.
+this._marker.on('click', this._onClick, this);
+this._map
+.on('click', this._onClick, this)
+.on('drag', onDragging)
+.addLayer(this._marker);
+}
+else {
+this._marker.setLatLng(latlng);
+}
+},
 
-            this._layerGroup = new L.layerGroup([this._marker]).addTo(this._map);
-			this._map.on('click', this._onClick, this);
-		}
-		else {
-			this._marker.setLatLng(latlng);
-		}
-	},
+_onClick: function (e) {
+this._map.fire(
+'draw:marker-created',
+{ marker: new L.Marker( this._marker.getLatLng(), {draggable:true}, { icon: this.options.icon }) }
+);
+this.disable();
+}
 
-	_onClick: function (e) {
-        var newMarker = new L.Marker(this._marker.getLatLng(), { icon: this.options.icon });
-		this._map.fire(
-			'draw:marker-created',
-			{ marker: newMarker }
-		);
-        newMarker.on('click', function () {
-            this._map.removeLayer(this._layerGroup);
-            this.enable();
-        }.bind(this), this);
-
-		this.disable();
-	}
-});
+}
+);
